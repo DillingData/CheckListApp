24d674a8c917fac77fb84130a8f2f0f8ca92e75c
Thomas <thomas@dillingdata.se>
2024-04-10 07:17:26 +0200

    Started work on getting the chosen checklist from the database

--- components/ManageChecklist/EditOrDeleteList.tsx
+++ components/ManageChecklist/EditOrDeleteList.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet, ScrollView } from "react-native";
+import { useState, useEffect } from 'react';
+import * as SQLite from 'expo-sqlite';
 
 class CheckList {
     public id: number | undefined;
@@ -7,8 +9,18 @@ class CheckList {
 }
 
 const EditOrDeleteList = ({route, navigation}:any) => {
-    
     const { Table } = route.params;
+    const [isLoading, setIsLoading] = useState(true);
+    const [chosenChecklist, setChecklist] = useState<CheckList[]>([]);
+    const tableName: string = JSON.stringify(Table);
+
+    const loadData = () => {
+        const db = SQLite.openDatabase('AllCheckLists.db');
+
+        db.transaction(query => {
+            query.executeSql('SELECT * FROM ' + tableName + '',)
+        })
+    }
 
     return (
         <View>
