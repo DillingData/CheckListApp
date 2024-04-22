0608423d8c69a9731acc66ac895cb90ab6ba2c11
Thomas <thomas@dillingdata.se>
2024-04-22 18:30:49 +0200

    Adding feature to check if the checklist is already started

--- components/ActivateChecklist/ActivateList.tsx
+++ components/ActivateChecklist/ActivateList.tsx
@@ -16,14 +16,48 @@ class ActivatedChecklist {
     public COMPLETED: boolean | undefined;
 }
 
+function CheckIfExists(incomingTable: string | undefined) {
+    const dbActive = SQLite.openDatabase('ActiveCheckLists.db');
+    const [active, setActive] = useState<ActivatedChecklist[]>([]);
+
+    dbActive.transaction(query => {
+        try {
+            query.executeSql('SELECT * FROM ' + incomingTable + '', [], 
+                (_, {rows: {_array} }) => {
+                    setActive(_array);
+                }
+            )
+        }
+        catch (error) {
+            console.log(error);
+        } 
+    })
+
+    if (active.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 const Activate = ({route, navigation}:any) => {
     let tableName: string | undefined;
     let tableName2: string | undefined;
     const [active, setActive] = useState<ActivatedChecklist[]>([]);
     const [chosen, setChosen] = useState<ChosenChecklist[]>([]);
+
+    const check = CheckIfExists(tableName);
+
+    if (check === true) {
+        Alert.alert('Checklist already started');
+        //Add button to navigation to go back
+        //navigation.goBack();
+    }
+
     const dbAll = SQLite.openDatabase("AllCheckLists.db");
     const dbActive = SQLite.openDatabase("ActiveCheckLists.db");
 
+    /*
     if (route.params != undefined) {
         const { Table } = route.params;
         tableName = JSON.stringify(Table).replace(/ /g, '_');
@@ -47,15 +81,22 @@ const Activate = ({route, navigation}:any) => {
 
         dbActive.transaction(query => {
             try {
-                query.executeSql('CREATE TALBE IF NOT EXISTS ' + tableName + '(ID INTEGER PRIMARY KEY AUTOINCREMENT, TASK TEXT, COMPLETED INT)');
+                query.executeSql('CREATE TABLE IF NOT EXISTS ' + tableName + '(ID INTEGER PRIMARY KEY AUTOINCREMENT, TASK TEXT, COMPLETED INT)');
             }
             catch (error) {
                 console.log(error);
             }
 
-            
+            for (let counter = 0; counter < chosen.length; counter++) {
+                try {
+                    query.executeSql('INSERT INTO') 
+                } catch(error) {
+                    console.log(error)
+                }
+            }
         })
     }
+    */
 
     console.log('tablName:' + tableName);
     console.log('tableName2: ' + tableName2);
